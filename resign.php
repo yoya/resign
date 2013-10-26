2bfac45869e931093b52867a9ecab339c4821638
yoya <yoya@sakurai.awm.jp>
2013-10-27 03:25:57 +0900

    - image storage

--- resign.php
+++ resign.php
@@ -1,6 +1,84 @@
 <?php
 
-if (isset($_REQUEST['ext']) && ($_REQUEST['ext'] === '.pdf')) {
+$query_string = $_SERVER['QUERY_STRING'];
+//var_dump($query_string);
+
+function image_data2id($data, $ext) { // 16 chars digest
+    $sha1 = sha1($data, true);
+    $base64 = base64_encode(substr($sha1, 0, 12));
+    return strtr($base64, '+/', '-_').'.'.$ext;
+}
+function image_id2dir($id) {
+    return "img/".substr($id, 0, 2);
+}
+
+function image_id2path($id) {
+   return image_id2dir($id)."/".$id; 
+}
+function image_id2origpath($id) {
+   return image_id2dir($id)."/_".$id; 
+}
+
+function image_id_valid($id) {
+    if (preg_match('/^[A-Za-z0-9\-\_]{16}\.(png|gif|jpg)$/', $id, $dummy) === 1) {
+        return true;
+    }
+    return false;
+}
+
+function build_queryparam($params) {
+   foreach ($params as $key => $value) {    
+       $param_peers[] = urlencode($key)."=".urlencode($value);
+   }
+   return join('&', $param_peers);
+}
+function alphabrend($data, $ext, $outfile) { // to white
+    $im = ImageCreateFromString($data);
+    imagefilter ($im, IMG_FILTER_BRIGHTNESS, 140);
+    switch ($ext) {
+        case 'jpg':
+            imagejpeg($im, $outfile);
+            break;
+        case 'png':
+            imagepng($im, $outfile);
+            break;
+        case 'gif':
+            imagegif($im, $outfile);
+            break;
+    }
+    ImageDestroy($im);
+}
+
+if (isset($_FILES['image_file']) && ($_FILES['image_file'] !== '')) {
+   $image_file = $_FILES['image_file'];
+   $tmp_name = $image_file['tmp_name'];
+   $image_data = file_get_contents($tmp_name);
+   if (strncmp($image_data, "\xff\xd8\xff", 3) == 0) {
+       $ext = 'jpg';
+   } else if (strncmp($image_data, "\x89PNG", 4) == 0) {
+       $ext = 'png';
+   } else if (strncmp($image_data, 'GIF', 3) == 0) {
+       $ext = 'gif';
+   }
+   $image_id = image_data2id($image_data, $ext);
+   $dir = image_id2dir($image_id);
+   if (file_exists($dir) === false) {
+        mkdir($dir);
+   }
+   $origpath = image_id2origpath($image_id);
+   $path = image_id2path($image_id);
+   if (file_exists($path) === false) {
+       file_put_contents($orig_path, $image_data);
+        alphabrend($image_data, $ext, $path);
+   }
+   //
+   $params = $_GET;
+   $params['image_id'] = $image_id;
+   $params['do'] = 'make';
+   $param_str = build_queryparam($params);
+   header("Location: ?$param_str");
+   exit(0);
+} elseif (isset($_REQUEST['ext']) && ($_REQUEST['ext'] === '.pdf')) {
     function date2japanize($date) {
         $from = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
         $to = array('〇', '一', '二', '三', '四', '五', '六', '七', '八', '九');
@@ -31,12 +109,21 @@ if (isset($_REQUEST['ext']) && ($_REQUEST['ext'] === '.pdf')) {
     $campany = $_REQUEST['campany'];
     $president = $_REQUEST['president'];
     $note = $_REQUEST['note'];
+    $image_id = $_REQUEST['image_id'];
 
 //    echo "$resign_date"; exit(0)
     require('yoyapdf.php');
     $pdf=new YoyaPDF();
     $pdf->AddSJISFont(); // XXX
     $pdf->AddPage();
+
+    if (($image_id !== '') && image_id_valid($image_id)) {
+       $path = image_id2path($image_id);
+       if (file_exists($path)) {
+           $pdf->RotatedImage($path, 30, 50, 140, 200,0);
+       }
+    }
+
     $pdf->SetFont('SJIS','',52);
     $x = 170;
     if ($type === 'wish')  {
@@ -147,6 +234,7 @@ $form = array(
     array('自分の名前', 'text', 'myname', ''),
     array('会社名', 'text', 'campany', 'ダミー株式会社'),
     array('社長', 'text', 'president', '代表取締役 山田太郎様'),
+    array('画像ID', 'text', 'image_id', ''),
     array('申し送り', 'textarea', 'note', ''),
 );
 
@@ -163,7 +251,7 @@ foreach ($form as $form_elem) {
     list($title, $type, $label, $value) = $form_elem;
     if ($type === 'textarea') {
 echo "<tr><th>$title</th> <td>
-<textarea name=\"$label\" rows=\"4\" cols=\"30\">$value</textarea></td></tr>\n";
+<textarea name=\"$label\" rows=\"4\" cols=\"30\" style=\"width:100%\">$value</textarea></td></tr>\n";
     } elseif ($type === 'radio') {
         $chcked_value = $form_elem[3];
         $type_list = array_slice($form_elem, 4);
@@ -180,17 +268,26 @@ echo "<tr><th>$title</th> <td>
         echo " </td></tr>\n";
     } else {
         echo "<tr><th>$title</th>
-<td><input type=\"$type\" name=\"$label\"  value=\"$value\" /></td></tr>\n";
+<td><input type=\"$type\" name=\"$label\" value=\"$value\" style=\"width:100%\" />";
+        echo "</td></tr>\n";
     }
 }
 
-echo <<< MIDDLE
+echo <<< MIDDLE1
 </table>
 <br/>
-
 <p>
 <input type="submit" name="do" value="make" class="btn btn-primary" />&nbsp;&nbsp;<input type="submit" name="do" value="reset" class="btn btn-warning" />
 </p>
+</form>
+<hr>
+MIDDLE1;
+echo "<form enctype=\"multipart/form-data\" action=\"?".$query_string."\" method=\"POST\">\n";
+echo <<< MIDDLE2
+<input type="hidden" name="MAX_FILE_SIZE" value="100000000" />
+ 壁紙用の画像ファイルをアップロード: <input name="image_file" type="file" class="btn btn-success" />
+<input type="submit" value="画像ファイル送信" class="btn btn-primary" />
+</form>
 <hr>
 ※ 名前は空欄にして直筆でのサインを推奨します。</br>
 ※ 名前の下に印鑑を忘れないでね☆</br>
@@ -198,15 +295,15 @@ echo <<< MIDDLE
 <hr>
 </td>
 <td width="540px" height="800px">
-MIDDLE;
+MIDDLE2;
 
 $params = array();
 foreach ($form as $idx => $form_elem) {    
     list($title, $type, $label, $value) = $form_elem;
-    $params[] = $label."=".urlencode($value);
+    $params[$label] = $value;
 }
-$params[] = 'ext=.pdf';
-$param_str = join('&', $params);
+$params['ext'] = '.pdf';
+$param_str = build_queryparam($params);
 echo "<iframe src=\"?$param_str\" name=\"pdf\" width=\"100%\" height=\"100%\"></iframe>\n";
 
 echo <<< FOOT
